d9fc8bd349186301cb24a5d2fea8a6899f5c1d28
OneWizzard <kurniawanrachmadhani@gmail.com>
2024-01-26 20:20:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+<div class="login-container">
+<h2>Login</h2>
+<form action="login.php" method="post">
+<div class="input-group">
+    <button type="submit" name="admin_login" value="Login as Admin">Login as Admin</buttton>
+</div>
+<div class="input-group">
+    <button type="submit" name="guest_login" value="Login as Dosen/Mahasiswa">Login as Dosen/Mahasiswa</button>
+</div>
+</form>
+</div>
+</body>
+</html>
+</html>
