b13d8c97728847e73613d33901c17c82dfbc3c31
Scklütz <138639029+zakirio-hw@users.noreply.github.com>
2024-01-27 17:54:20 +0700

    Update conn.php - Zakirio

--- conn.php
+++ conn.php
@@ -2,8 +2,8 @@
     $host = 'sql6.freesqldatabase.com';
     $user = 'sql6680060';
     $pass = 'gUmGNVk9pE';
-    $name = 'sql6680060';
-    $conn = mysqli_connect($host, $user, $pass, $name);
+    $db = 'sql6680060';
+    $conn = mysqli_connect($host, $user, $pass, $db);
 
     if (!$conn) {
         die("Koneksi error: ".mysqli_connect_error());
