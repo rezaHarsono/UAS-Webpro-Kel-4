b6f666b0571da8444d201a02c4461e8c51f24460
Muhammad Reza Harsono <reza220304@gmail.com>
2024-01-27 16:21:01 +0700

    Mengintegrasikan fitur login dengan database

--- login.php
+++ login.php
@@ -1,33 +1,50 @@
 <?php
+include_once('conn.php');
+
 // Fungsi untuk memeriksa login admin
-function adminLogin($username, $password) {
+function adminLogin($username, $password)
+{
+    global $conn;
     // Gantilah ini dengan logika pemeriksaan login admin sesuai kebutuhan
-    return $username === 'admin' && $password === 'admin';
+    $stmt = $conn->prepare("SELECT * FROM admin WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+
+    return $result->num_rows === 1;
 }
 
 // Fungsi untuk memeriksa login user
-function userLogin($username, $password) {
+function userLogin($username, $password)
+{
+    global $conn;
     // Gantilah ini dengan logika pemeriksaan login user sesuai kebutuhan
-    return $username === 'user' && $password === 'user';
+    $stmt = $conn->prepare("SELECT * FROM dosen WHERE nama = ? AND id = ?");
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+
+    return $result->num_rows === 1;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-     // Pemeriksaan login admin
-     if (adminLogin($username, $password)) {
+    // Pemeriksaan login admin
+    if (adminLogin($username, $password)) {
         header("Location: admin.php");
         exit();
-    } 
+    }
     // Pemeriksaan login user
     elseif (userLogin($username, $password)) {
         header("Location: user.php");
         exit();
-    } 
+    }
     // Jika login gagal
     else {
         echo "Login gagal. Silakan coba lagi.";
     }
 }
-?>
