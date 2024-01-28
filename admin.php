2a920345c8868fabcf3dd902407766d2efaa1de9
salmaakmll <salmakamilaarsipkuliah@gmail.com>
2024-01-28 23:46:45 +0700

    benerin log out

--- admin.php
+++ admin.php
@@ -61,15 +61,8 @@ if (!$result) {
 
   <div class="navbar">
     <h2 class="admin-title">Admin</h2>
-    <button class="logoutbtn"><a class="logout" href="logout.php">LogOut</a></button>
-    <div class="dropdown">
-      <button class="dropbtn">PILIH SECTION</button>
-      <div class="dropdown-content">
-        <a href="#">SHOW ALL</a>
-        <a href="#">GEDUNG GSG</a>
-        <a href="#">GEDUNG AA</a>
-      </div>
-    </div>
+    <button class="logoutbtn">
+        <a class="logout" href="logout.php">LogOut</a></button>
   </div>
 
   <div class="title">
