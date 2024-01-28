746f601cbf05f3e429dab738959a3864b5992726
OneWizzard <kurniawanrachmadhani@gmail.com>
2024-01-28 14:41:29 +0700

    TAMPILAN USER

--- user.php
+++ user.php
@@ -3,11 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="user.css">
     <title>User</title>
 </head>
 <body>
-
-<h2>User</h2>
+<div class="navbar">
+    <h2 class="user-title">User</h2>
+    <div class="dropdown">
+        <button class="dropbtn">PILIH SECTION</button>
+        <div class="dropdown-content">
+            <a href="#">SHOW ALL</a>
+            <a href="#">GEDUNG GSG</a>
+            <a href="#">GEDUNG AA</a>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
