b6c2e2b1fd2454bb9c29d12f6edc5e1e3280da35
KasamirAnindita <kasamira.anindita.qairia.tik22@mhsw.pnj.ac.id>
2024-01-28 22:14:02 +0700

    css button tambah

--- user.php
+++ user.php
@@ -17,12 +17,6 @@ if (!$result) {
   <link rel="stylesheet" href="user.css">
   <title>User</title>
 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="user.css">
-    <title>User</title>
-
     <style>
     table {
       margin: auto;
